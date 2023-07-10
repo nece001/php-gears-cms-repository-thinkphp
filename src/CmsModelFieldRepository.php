6de7ac886880f24b7158de92c37ea17bf8ee63cf
nece001 <nece001@163.com>
2023-07-10 21:13:06 +0800

    增加字段类

---
+++ src/CmsModelFieldRepository.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Nece\Gears\Cms\Repository\ThinkPHP;
+
+use Nece\Gears\Cms\Entity\CmsModelFieldEntity;
+use Nece\Gears\Cms\Repository\ICmsModelFieldRepository;
+use Nece\Gears\Cms\Repository\ThinkPHP\Model\CmsModelField;
+
+class CmsModelFieldRepository implements ICmsModelFieldRepository
+{
+    /**
+     * 查询模型字段
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-07-08
+     *
+     * @param string $id
+     *
+     * @return CmsModelFieldEntity|null
+     */
+    public function find($id)
+    {
+        $item = CmsModelField::find($id);
+        if ($item) {
+            $entity = new CmsModelFieldEntity();
+            $entity->id = $item->id;
+            $entity->title = $item->title;
+            $entity->create_time = $item->create_time;
+            $entity->update_time = $item->update_time;
+            $entity->is_disabled = $item->is_disabled;
+            $entity->sort = $item->sort;
+            $entity->definition_id = $item->definition_id;
+            $entity->search_type = $item->search_type;
+            $entity->value_type = $item->value_type;
+            $entity->value_format = $item->value_format;
+            return $entity;
+        }
+
+        return null;
+    }
+
+    /**
+     * 创建或更新模型字段
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-07-08
+     *
+     * @param CmsModelFieldEntity $entity
+     *
+     * @return void
+     */
+    public function createOrUpdate(CmsModelFieldEntity $entity)
+    {
+        if ($entity) {
+            if ($entity->id) {
+                $model = CmsModelField::find($entity->id);
+            } else {
+                $model = new CmsModelField();
+            }
+
+            $model->title = $entity->title;
+            $model->is_disabled = $entity->is_disabled ? 1 : 0;
+            $model->sort = $entity->sort;
+            $model->definition_id = $entity->definition_id;
+            $model->search_type = $entity->search_type;
+            $model->value_format = $entity->value_format;
+            $model->save();
+        }
+    }
+
+    /**
+     * 删除模型字段
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-07-08
+     *
+     * @param CmsModelFieldEntity $definition
+     *
+     * @return void
+     */
+    public function delete(CmsModelFieldEntity $definition)
+    {
+        CmsModelField::where('id', $definition->id)->delete();
+    }
+}
